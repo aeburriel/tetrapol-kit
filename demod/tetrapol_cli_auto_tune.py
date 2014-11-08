e6905aca819373c077a9f422ee71539688026952
Jiri Svoboda <jiri.svoboda@yandex.com>
2014-11-08 18:31:34 +0100

    tool for setting auto-tunning channel

---
+++ demod/tetrapol_cli_auto_tune.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Get/set channel for automatic frequency fine tunning.
+
+Usage: tetrapol_cli_auto_tune.py [CH_NO]"""
+
+from xmlrpc import client
+import sys
+
+c = client.Server("http://localhost:60100")
+if len(sys.argv) == 2:
+    c.set_auto_tune([int(sys.argv[1])])
+else:
+    print(c.get_auto_tune())
