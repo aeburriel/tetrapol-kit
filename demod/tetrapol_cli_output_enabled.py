fb7eb14a5c15856cc804b16760915b8ff2610cf6
Jiri Svoboda <jiri.svoboda@yandex.com>
2014-11-17 14:17:15 +0100

    make RX channels unique

--- demod/tetrapol_cli_output_enabled.py
+++ demod/tetrapol_cli_output_enabled.py
@@ -8,12 +8,12 @@ from xmlrpc import client
 import sys
 
 c = client.Server("http://localhost:60100")
-if sys.argv[1] in ("1", "true", "enable", "open", "on"):
+if sys.argv[1] in ("true", "enable", "open", "on"):
     enabled = True
-elif sys.argv[1] in ("0", "false", "disable", "close", "off"):
+elif sys.argv[1] in ("false", "disable", "close", "off"):
     enabled = False
 else:
     raise ValueError("Invalid parameter")
-channels = [int(c) for c in sys.argv[2:]]
+channels = list(set([int(c) for c in sys.argv[2:]]))
 c.set_output_enabled(channels, enabled)
 
