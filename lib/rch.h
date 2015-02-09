1d0a6f413cb2ef777513a0ba33d4e0d4ec54ff85
Jiri Svoboda <jiri.svoboda@yandex.com>
2015-02-09 16:18:24 +0100

    RCH infrastructure

---
+++ lib/rch.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "data_block.h"
+
+#include <stdbool.h>
+
+typedef struct _rch_t rch_t;
+
+rch_t *rch_create(void);
+void rch_destroy(rch_t *rch);
+bool rch_push_data_block(rch_t *rch, data_block_t *data_blk);
+void rch_print(const rch_t *rch);
+
