69d5cd3039497d03410a95fcc9c8af3357a807e4
Jiri Svoboda <jiri.svoboda@yandex.com>
2015-02-15 20:39:42 +0100

    handle ACK_DACH cmd

--- lib/sdch.c
+++ lib/sdch.c
@@ -112,6 +112,22 @@ bool sdch_dl_push_data_frame(sdch_t *sdch, data_block_t *data_blk)
         return false;
     }
 
+    if (sdch->hdlc_fr->command.cmd == COMMAND_DACH) {
+        IF_LOG(INFO) {
+            LOG_("\n\tcmd ACK_DACH\n\taddr: ");
+            addr_print(&sdch->hdlc_fr->addr);
+            printf("\n");
+        }
+        if (!cmpzero(sdch->hdlc_fr->data, sdch->hdlc_fr->nbits / 8)) {
+            IF_LOG(WTF) {
+                LOG_("cmd: ACK_DACH, nonzero stuffing");
+                print_hex(sdch->hdlc_fr->data, sdch->hdlc_fr->nbits / 8);
+            }
+        }
+        // TODO: report ACK_DACH to application layer
+        return false;
+    }
+
     LOG(INFO, "old CMD 0x%02x", sdch->hdlc_fr->command.cmd);
 
     // TODO ...
